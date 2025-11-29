87aed1d78937e5a52150da9f065a375815ae6094
KerrickShiu <kshiu8@my.centennialcollege.ca>
2025-11-28 22:33:29 -0500

    more fix

--- server/express.js
+++ server/express.js
@@ -26,7 +26,7 @@ app.use("/api/users", userRoutes);
 
 app.use(express.static(path.join(__dirname, "client", "dist")));
 
-app.get("/*", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 });
 
